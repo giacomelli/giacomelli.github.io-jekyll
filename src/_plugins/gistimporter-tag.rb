2c8c1bd041dbdaecef8b599eade6804cb1bfa71e
giacomelli <giacomelli@gmail.com>
2019-07-06 18:57:45 -0300

    Fix filename case

--- src/_plugins/gistimporter-tag.rb
+++ src/_plugins/gistimporter-tag.rb
@@ -9,7 +9,7 @@ module Jekyll
       "<link rel='stylesheet' href='https://github.githubassets.com/assets/gist-embed-a9a1cf2ca01efd362bfa52312712ae94.css'><div class='gist' style='margin-top:-17px'>
         <div class='gist-file'>
           <div class='gist-meta'>
-            <a href='/unitytips-gist-importer/'>You can easily import this gist directly into your Unity 3D project using the <b>Gist Importer</b><img style='float:right;width:48px;' src='/assets/logos/unity3d.png' /></a></div>
+            <a href='/unitytips-gist-importer/'>You can easily import this gist directly into your Unity 3D project using the <b>Gist Importer</b><img style='float:right;width:48px;' src='/assets/logos/Unity3d.png' /></a></div>
         </div>
       </div>"
     end
