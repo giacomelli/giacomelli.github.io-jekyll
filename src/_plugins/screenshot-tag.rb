259c0269c6a0817a0dc0b0e08f79982a96dc3a19
giacomelli <giacomelli@gmail.com>
2020-09-12 10:24:51 -0300

    Added border to screenshots

--- src/_plugins/screenshot-tag.rb
+++ src/_plugins/screenshot-tag.rb
@@ -13,7 +13,7 @@ module Jekyll
 
       site = context.registers[:site]
       "<center>
-        <img class='lazy' src='#{site.baseurl}/assets/#{@text}'>
+        <img class='lazy screenshot' src='#{site.baseurl}/assets/#{@text}'>
       </center>"
     end
   end
